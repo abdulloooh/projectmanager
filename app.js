d595c592e2e09bcc56d69c6a54df7ac6099e5dea
abdulloooh <abdullahakinwumi@gmail.com>
2020-01-17 03:18:11 +0100

    Implemented add and delete project

---
+++ app.js
@@ -0,0 +1,34 @@
+var project_count = 0;
+var storage = {};
+// var toDoProjcect = document.getElementById("project_option");
+// var project = document.getElementById("project");
+
+function treatProject(){
+    let toDoProjcect = document.getElementById("project_option");
+    let opt = toDoProjcect.options[toDoProjcect.selectedIndex];
+    let project = document.getElementById("project").value;
+    switch(opt.value){
+        case "add":
+            //for add
+            storage[project]= []
+            console.log(opt.value);
+            console.log(storage);
+            break;
+        case "delete":
+            //for delete
+            delete a[project];
+            break;
+        default:
+            break;
+        }
+    }
+
+    function treatAction(){
+        //waht action to perform
+        let toDoAction = document.getElementById("action_option");
+        let opt = toDoAction.options[toDoAction.selectedIndex];
+        //what project to perform it on
+        let onProject = document.getElementById("projects");
+        let optP = onProject.options[onProject.selectedIndex];
+        let action = document.getElementById("project").value;
+    }
